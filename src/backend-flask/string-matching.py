26ef5344682d9e25da632c6a3f72604b118f1ecf
raisvaza <raisvaza@gmail.com>
2021-04-27 23:39:53 +0700

    kmp

--- src/backend-flask/string-matching.py
+++ src/backend-flask/string-matching.py
@@ -1,5 +1,3 @@
-kmp_table = []
-
 def kmp_border_function(pattern,j):
     k = j-1
     if k < 0:
@@ -19,20 +17,37 @@ def kmp_border_function(pattern,j):
         for j in range(k,0,-1):
             suffix = pattern[j] + suffix
             suffix_array.append(suffix)
-            print(suffix)
         for l in range(len(prefix_array)):
             if prefix_array[l] == suffix_array[l]:
                 to_return = len(prefix_array[l])
+        print(prefix_array)
+        print(suffix_array)
         return to_return
 
+def make_kmp_table(kmp_table,pattern):
+    for i in range(len(pattern)):
+        kmp_table.append(kmp_border_function(pattern,i))
 
+def kmp(text,pattern,kmp_table):
+    make_kmp_table(kmp_table,pattern)
+    i = 0
+    j = 0
+    # for i in range(len(text)):
+    while text[i] == pattern[j] and j < len(pattern) and i < len(text):
+        i += 1
+        j += 1
+    # mismatch or j = len(pattern) or i = len(text)
+    if text[i] != pattern[j]:
+        if j == 0:
+            i += 1
+        else:
+            j = kmp_table[j]
+    elif j == len(pattern): # matching
+        return i - j
+    else: # no match
+        return -1
 
-def is_same(string1,string2):
-    # exactmatch
-    if string1 == string2:
-        return True
-    else:
-        return False
-
-# for i in range(len(pattern)):
-#     kmp_table.append(kmp_border_function(pattern,i))
+kmp_table = []
+pattern = "abaaba"
+text = "bacbabaabaabcbca"
+print(kmp(text,pattern,kmp_table))
