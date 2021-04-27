734907104395d193dfdff3891e631034ad09a57e
raisvaza <raisvaza@gmail.com>
2021-04-28 01:38:28 +0700

    findTopik v2

--- src/backend-flask/command-processing.py
+++ src/backend-flask/command-processing.py
@@ -8,21 +8,6 @@ def findTopik(text):
     topik = re.search("\".*\"",text)
     return topik[0]
 
-if ("""ada TANGGAL, MATKUL, JENIS, TOPIK"""):
-    # nambah task baru
-elif ("""ada APA, DEADLINE, ((DATE1 & DATE2)/(N & (HARI/MINGGU) & DEPAN)/HARI INI)/[jenis task]"""):
-    # nampilin daftar task
-elif ("""ada DEADLINE, MATKUL"""):
-    # nampilin deadline tugas
-elif ("""ada ID, UNDUR/MAJU, KODE MATKUL, TANGGAL"""):
-    # memperbarui task
-elif ("""ada SELESAI/BERES, ID TASK"""):
-    # menandai task selesai
-elif ("""ada ASSISTANT, BISA, APA"""):
-    # menampilkan opsi help
-else:
-    print("Maaf, pesan tidak dikenali")
-
 def findTask(kalimat):
     if (findWord(kalimat, "tubes"):
         return "Tubes"
@@ -35,3 +20,17 @@ def findTask(kalimat):
     elif (findWord(kalimat, "praktikum")):
         return "Praktikum"
 
+# if ("""ada TANGGAL, MATKUL, JENIS, TOPIK"""):
+#     # nambah task baru
+# elif ("""ada APA, DEADLINE, ((DATE1 & DATE2)/(N & (HARI/MINGGU) & DEPAN)/HARI INI)/[jenis task]"""):
+#     # nampilin daftar task
+# elif ("""ada DEADLINE, MATKUL"""):
+#     # nampilin deadline tugas
+# elif ("""ada ID, UNDUR/MAJU, KODE MATKUL, TANGGAL"""):
+#     # memperbarui task
+# elif ("""ada SELESAI/BERES, ID TASK"""):
+#     # menandai task selesai
+# elif ("""ada ASSISTANT, BISA, APA"""):
+#     # menampilkan opsi help
+# else:
+#     print("Maaf, pesan tidak dikenali")
