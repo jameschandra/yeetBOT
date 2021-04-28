2411dbcada9a6c549d534aff4ad6706557e3fa81
MDrFaza <mdrfaza21@gmail.com>
2021-04-28 14:46:31 +0700

    fix newline in messageResponse

--- src/frontend-react/src/components/MessageResponse.js
+++ src/frontend-react/src/components/MessageResponse.js
@@ -4,7 +4,10 @@ import '../styles/messageresponse.css';
 const MessageResponse = ({ message }) => {
     return (
         <div className="response-box">
-            <p className="response-message">{message}</p>
+            <p className="response-message">
+            {message.split("\n").map((i,key) => {
+                return <span>{i}{<br />}</span>
+            })}</p>
         </div>
     )
 }
