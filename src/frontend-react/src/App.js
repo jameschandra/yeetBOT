b929a425f6ae50575e84df57b08a7ddee3faebd4
jamesteguh <tweenlicious@gmail.com>
2021-05-07 03:34:39 +0700

    change post request endpoint

--- src/frontend-react/src/App.js
+++ src/frontend-react/src/App.js
@@ -25,7 +25,7 @@ const App = () => {
   useEffect(() => { 
     if(!answered){
       axios
-        .post("https://yeetbot-backend-flask.herokuapp.com/chat", {
+        .post("chat", {
           messages: messages,
         })
         .then((res) => {
